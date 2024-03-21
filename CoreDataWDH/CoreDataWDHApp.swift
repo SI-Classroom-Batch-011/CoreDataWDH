fb5aa98eccc0ff9b2d0939cb01eee5ec7670a743
joshur19 <rueck.joshua@gmail.com>
2024-03-21 12:45:00 +0100

    Initial Commit

---
+++ CoreDataWDH/CoreDataWDHApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataWDHApp.swift
+//  CoreDataWDH
+//
+//  Created by Joshua Rück on 21.03.24.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataWDHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
